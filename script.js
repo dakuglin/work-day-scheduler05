16d041fb490b111ef1008991dac1f0d86ea7b076
Dana Kuglin <dakuglin@gmail.com>
2020-10-01 09:57:32 -0600

    added files and created script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() { 
+
+//$('currentDay').text(moment().format("MMM Do YY")); 
+$('#currentDay').text(moment().format('MMMM Do YYYY'));
+//$('#currentDay').append()
+
+// alert("Is this working")
+
+});
