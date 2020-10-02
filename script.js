f95f4c4ad1886f1170b66979dbfa9753b6fe2150
Dana Kuglin <dakuglin@gmail.com>
2020-10-02 09:45:00 -0600

    updated files

--- script.js
+++ script.js
@@ -1,70 +1,71 @@
 
-$(document).ready(function() { 
+$(document).ready(function() {   //goes in every jQuery script
 
     //display current date in p tag on html
     $('#currentDay').text(moment().format('MMMM Do YYYY'));
 
 
-    function timeblockColor() {
+     //each out on schedule military time
+   
+
+    function timeblockColor () {
 
         var time = new Date(); //current time
         var militaryTime = time.getHours(); //military time (just hour)
         console.log(militaryTime);
-        
 
-        var row =  $(".hour9").text(); //9am
-        console.log(row)
-        var hourNine = parseInt(row);
-        var TEST = time.hourNine;
-        console.log(TEST)
-        console.log(hourNine);
-
-        if (militaryTime < hourNine ) {  //current time is less than 
-         
-        //    var test =  $(".hour9").find("textarea")
-        //    console.log(test)
-            $(".hour9").hide()
-        
-
-               
-        } else if (militaryTime === hourNine) {
-
-            //$(".hour9").find("textarea").addClass(".present")
-            $(".hour9").hide()
-
-        } else if (militaryTime > hourNine) {
-            console.log("true")
-
-            //$("hour9 > textarea").addClass("past")
-            //$(".hour9").find("textarea").addClass(".present")
-            //$(".description9").css('color','red')
-        //    var test3 =  $("#hour-9").find("textarea").addClass('.past');
-        //    $(".description9").append(test3)
-
-        
-            //var test = $(".description9").css("background-color","red")
-             var test = $(".description9").css("display" ,"visible")
-            .past
-            $(".description9").append(test)
-        }
-    };
+        var hours = ["9","10","11","12","13","14","15","16","17"];
+        var hourIndex = 0; //index of hours array
     
+        for (var i=0; i < hours.length; i++) {
+            if (militaryTime < hours[hourIndex] ) {         //current time is less than 
+                console.log("future")                       //future
+                $(".description").addClass("future");
+                hourIndex++;
+                console.log(hours[hourIndex])
+                 
+            } else if (militaryTime === hours[hourIndex]) { //current time is equal to
+                console.log("present")                      //present
+                $(".description").addClass("present");
+                hourIndex++;
+        
+            } else if (militaryTime > hours[hourIndex]) {   //current time is greater than 
+                console.log("past")                         //past
+                $(".description").addClass("past");
+                hourIndex++;
+            }
+        }    
+    }
+
     timeblockColor();
-
-
-    // var test = $(".saveBtn").css("background-color","red")
-
-    // $(".saveBtn").append(test)
- 
-    //focus on data element firlst
-
-    //only thing we need date library for is how to tell the time element with the css and at top of screen
-
-    // how will wil store this data, how to write it to local storage, how to get it out of local storage 
 
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//focus on data element firlst
+
+    //only thing we need date library for is how to tell the time element with the css and at top of screen
+
+    // how will wil store this data, how to write it to local storage, how to get it out of local storage 
 
 
 //Pseduo Code
