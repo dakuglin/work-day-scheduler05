daa4667e733dbd75004c16bd01617cda19e9da41
Dana Kuglin <dakuglin@gmail.com>
2020-10-02 06:47:48 -0600

    updated files

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # work-day-scheduler05
+
+# 05 Third-Party APIs: Work Day Scheduler
+
+Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+
+## User Story
+
+```
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+* Pseudo Code
+
+    * User opens the browser to the day planner
+        * Current date is displayed at the top
+
+            * Time blocks are on the screen from 9am to 5pm
+                * color shading 
+                * blocks past current time are grey
+                * block on current hour is green
+                * blocks in future are red
+
--- script.js
+++ script.js
@@ -1,10 +1,82 @@
 
 $(document).ready(function() { 
 
-//$('currentDay').text(moment().format("MMM Do YY")); 
-$('#currentDay').text(moment().format('MMMM Do YYYY'));
-//$('#currentDay').append()
+    //display current date in p tag on html
+    $('#currentDay').text(moment().format('MMMM Do YYYY'));
 
-// alert("Is this working")
+
+    function timeblockColor() {
+
+        var time = new Date(); //current time
+        var militaryTime = time.getHours(); //military time (just hour)
+        console.log(militaryTime);
+        
+
+        var row =  $(".hour9").text(); //9am
+        console.log(row)
+        var hourNine = parseInt(row);
+        var TEST = time.hourNine;
+        console.log(TEST)
+        console.log(hourNine);
+
+        if (militaryTime < hourNine ) {  //current time is less than 
+         
+        //    var test =  $(".hour9").find("textarea")
+        //    console.log(test)
+            $(".hour9").hide()
+        
+
+               
+        } else if (militaryTime === hourNine) {
+
+            //$(".hour9").find("textarea").addClass(".present")
+            $(".hour9").hide()
+
+        } else if (militaryTime > hourNine) {
+            console.log("true")
+
+            //$("hour9 > textarea").addClass("past")
+            //$(".hour9").find("textarea").addClass(".present")
+            //$(".description9").css('color','red')
+        //    var test3 =  $("#hour-9").find("textarea").addClass('.past');
+        //    $(".description9").append(test3)
+
+        
+            //var test = $(".description9").css("background-color","red")
+             var test = $(".description9").css("display" ,"visible")
+            .past
+            $(".description9").append(test)
+        }
+    };
+    
+    timeblockColor();
+
+
+    // var test = $(".saveBtn").css("background-color","red")
+
+    // $(".saveBtn").append(test)
+ 
+    //focus on data element firlst
+
+    //only thing we need date library for is how to tell the time element with the css and at top of screen
+
+    // how will wil store this data, how to write it to local storage, how to get it out of local storage 
 
 });
+
+
+
+
+//Pseduo Code
+
+//User opens the browser to the day planner
+    // Current date is displayed at the top
+
+    //Time blocks are on the screen from 9am to 5pm
+        //color shading 
+        //blocks past current time are grey
+        //block on current hour is green
+        //blocks in future are red
+
+
+
